6eb7fd764e420a90862491338a49cb759f9421ee
Dhanush GN <142598916+DhanushGN@users.noreply.github.com>
2025-06-18 13:58:25 +0530

    Update server.py

--- server.py
+++ server.py
@@ -41,5 +41,6 @@ def server_receive():
     except Exception as e:
         return jsonify({"status": "error", "message": str(e)}), 500
 
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
